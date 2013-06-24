a878df03fe88f598fc799409c1b1a8c5c269ba80
Yavor Georgiev <yavorg@microsoft.com>
2013-06-23 17:03:23 -0700

    CrapChat UI in progress

---
+++ CrapChat/CrapChat/ViewModel/PhotosViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrapChat.ViewModel
+{
+    public class PhotosViewModel : ViewModelBase
+    {
+ 
+    }
+}
---
+++ CrapChat/CrapChat/ViewModel/SendViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrapChat.ViewModel
+{
+    public class SendViewModel : ViewModelBase
+    {
+        private ViewModelLocator locator;
+
+        public SendViewModel()
+        {
+            locator = App.Current.Resources["Locator"] as ViewModelLocator;
+        }
+
+    
+        public Stream Image
+        {
+            get
+            {
+                return locator.Camera.Image;
+            }
+        }
+
+    }
+
+
+}
