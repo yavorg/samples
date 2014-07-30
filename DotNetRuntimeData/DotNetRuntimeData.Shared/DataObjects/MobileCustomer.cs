e8ffd3c0d6dab270edf0933ba147e82f5a96ebb4
Yavor Georgiev <yavorg@microsoft.com>
2014-07-30 10:27:13 -0700

    Switching client project to universal app and updating offline

---
+++ DotNetRuntimeData/DotNetRuntimeData.Shared/DataObjects/MobileCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetRuntimeData
+{
+    public class MobileCustomer
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [CreatedAt]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        [UpdatedAt]
+        public DateTimeOffset? UpdatedAt { get; set; }
+
+        public bool Deleted { get; set; }
+        
+        [Version]
+        public string Version { get; set; }
+
+    }
+
+}
